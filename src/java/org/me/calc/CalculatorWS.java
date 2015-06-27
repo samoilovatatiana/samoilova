7c288924af9dc66387a97821b66496f842f5b946
Самойлова <@Samoilova>
2015-06-27 10:33:31 +0400

    все хорошо

---
+++ src/java/org/me/calc/CalculatorWS.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.me.calc;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+/**
+ *
+ * @author User
+ */
+@WebService()
+public class CalculatorWS {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "add")
+    public int add(@WebParam(name = "i") int i, @WebParam(name = "j") int j) {
+        return i + j;
+    }
+
+}
